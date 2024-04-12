44dcdcc2eede2eb66b644ba101a3a7937f805d67
saraporri <saraporricino@yahoo.com>
2024-04-12 10:06:39 +0200

    hover add and heart

---
+++ ecommerce/src/app/pages/profile/profile.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { iUser } from '../../Models/iuser';
+import { ActivatedRoute } from '@angular/router';
+import { UsersService } from '../../users.service';
+import { AuthService } from '../../auth/auth.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.scss'
+})
+export class ProfileComponent {
+
+  user: iUser | undefined;
+
+  constructor(private authSvc: AuthService, private usersSvc: UsersService) { }
+
+  ngOnInit(): void {
+    this.getUser();
+  }
+
+  getUser(): void {
+    // Utilizza l'ID dell'utente autenticato per recuperare i dettagli dell'utente
+    const userId = this.authSvc.getCurrentUserId();
+    if (userId !== null) {
+      this.usersSvc.getUserById(userId).subscribe(user => this.user = user);
+    }
+  }
+  saveUser(): void {
+    if (this.user) {
+      this.usersSvc.updateUser(this.user).subscribe(() => {
+       console.log('Modifiche al profilo eseguite con successo');
+      });
+    }
+  }
+
+}
